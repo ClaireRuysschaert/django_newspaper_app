6f8785ff8b5fecf378247cbf76f70cd1d839f686
Claire Ruysschaert <netclaire@hotmail.fr>
2023-04-10 14:50:05 +0200

    page prettified with crispy forms

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd Party
+    "crispy_forms", 
+    "crispy_bootstrap5",
+    #Local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
 ]
@@ -129,3 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" 
+CRISPY_TEMPLATE_PACK = "bootstrap5" 
